a3bd701c221e9eee3aac52b8cca62dc50a43162e
zxt6154 <zxt61542990@yeah.net>
2022-05-15 20:10:09 +0800

    顺序表

--- 0201_SqList/SqList.h
+++ 0201_SqList/SqList.h
@@ -2,7 +2,18 @@
 // Created by alice on 2022/5/15.
 //
 
-#ifndef DATASTRUCTURE_SQLIST_H
-#define DATASTRUCTURE_SQLIST_H
+#ifndef SQLIST_H
+#define SQLIST_H
 
-#endif //DATASTRUCTURE_SQLIST_H
+#include <stdio.h>
+#include <stdlib.h>     //提供malloc, free, exit原型
+#include "Status.h"
+
+/* 宏定义 */
+#define LIST_INIT_SIZE 100  //顺序表存储空间的初始分配值
+#define LISTINCRMENT 10     //顺序表存储空间的分配增量
+
+/* 顺序表元素类型定义 */
+typedef  int ElemType;
+
+
