ea73bceb9c98c72de7abc1616b4a0da9f005e07c
zxt6154 <zxt61542990@yeah.net>
2022-05-16 15:36:37 +0800

    c语言学习

--- helloworld/Test.c
+++ helloworld/Test.c
@@ -3,6 +3,16 @@
 //
 #include <stdio.h>
 int main() {
-    printf("hello world");
+//    printf("hello world!\n");
+//    printf("12+34=%d", 12+34);
+    int price = 0;
+
+    printf("请输入金额： ");
+    scanf("%d", &price);
+
+    int change = 100 - price;
+
+    printf("找零%d元\n", change);
+
     return 0;
 }
