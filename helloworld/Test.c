9aa54125861b2af8ba3ffcd4e2531cbed4c0ce35
zxt6154 <zxt61542990@yeah.net>
2022-05-16 18:23:10 +0800

    c语言学习

--- helloworld/Test.c
+++ helloworld/Test.c
@@ -7,12 +7,48 @@ int main() {
 //    printf("12+34=%d", 12+34);
     int price = 0;
 
-    printf("请输入金额： ");
-    scanf("%d", &price);
+//    printf("请输入金额： ");
+//    scanf("%d", &price);
+//
+//    int change = 100 - price;
+//
+//    printf("找零%d元\n", change);
 
-    int change = 100 - price;
+    printf("------------------");
+    printf("\n");
+//
+//    long x;
+//    int n = 0;
+//
+//    scanf("%d", &x);
+//    n++;
+//    x /= 10;
+//    while (x > 0) {
+//        n++;
+//        x /= 10;
+//    }
+//    printf("%d\n", n);
 
-    printf("找零%d元\n", change);
+
+
+/**
+ * 素数
+ */
+    int x;
+    scanf("%d", &x);
+    int i;
+    int isPrime = 1;
+    for (i = 2; i < x; i++) {
+        if(x%i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+    if(isPrime == 1) {
+        printf("是素数");
+    } else {
+        printf("不是素数");
+    }
 
     return 0;
 }
