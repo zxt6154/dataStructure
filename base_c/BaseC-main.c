a400f760dae65d611547942302f641c3cfa33ad5
zxt6154 <zxt61542990@yeah.net>
2022-05-17 09:49:14 +0800

    about sushu

--- base_c/BaseC-main.c
+++ base_c/BaseC-main.c
@@ -5,38 +5,38 @@
  */
 #include <stdio.h>
 
-int main() {
-    int a;
-    scanf("%d", &a);
-    int i, j, k;
-    int cnt = 0;
-
-    i = a;
-    while (i <= a+3) {
-        j = a;
-        while (j <= a+3) {
-            k = a;
-            while (k <= a+3) {
-                if(i != j) {
-                    if(i != k) {
-                        if(j != k) {
-                            cnt++;
-                            printf("%d%d%d",i,j,k);
-                            if(cnt == 6){
-                                printf("\n");
-                                cnt = 0;
-                            } else {
-                                printf(" ");
-                            }
-                        }
-                    }
-                }
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
-
-    return 0;
-}
+//int main() {
+//    int a;
+//    scanf("%d", &a);
+//    int i, j, k;
+//    int cnt = 0;
+//
+//    i = a;
+//    while (i <= a+3) {
+//        j = a;
+//        while (j <= a+3) {
+//            k = a;
+//            while (k <= a+3) {
+//                if(i != j) {
+//                    if(i != k) {
+//                        if(j != k) {
+//                            cnt++;
+//                            printf("%d%d%d",i,j,k);
+//                            if(cnt == 6){
+//                                printf("\n");
+//                                cnt = 0;
+//                            } else {
+//                                printf(" ");
+//                            }
+//                        }
+//                    }
+//                }
+//                k++;
+//            }
+//            j++;
+//        }
+//        i++;
+//    }
+//
+//    return 0;
+//}
--- base_c/BaseC.c
+++ base_c/BaseC.c
@@ -4,3 +4,47 @@
 
 #include "BaseC.h"
 #include <stdio.h>
+
+int sushuSum();
+int isSushu(int i);
+
+int main () {
+    int sum = sushuSum();
+    printf("%d", sum);
+}
+
+
+
+int sushuSum() {
+    int m, n, i;
+    int cnt = 0;
+    int sum = 0;
+
+    scanf("%d %d", &m, &n);
+    for(i = m; i < n; i++) {
+        sum += isSushu(i);
+    }
+    return sum;
+}
+
+int isSushu(int num) {
+/**
+ * 素数
+ */
+    //   scanf("%d", &num);
+    int i;
+    int isPrime = 1;
+    for (i = 2; i < num; i++) {
+        if(num%i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+    if(isPrime == 1) {
+        printf("是素数");
+        return num;
+    } else {
+        printf("不是素数");
+        return 0;
+    }
+}
